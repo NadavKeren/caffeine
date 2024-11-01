af372c77363bb492f361fa5dc125e14793c65862
Nadav Keren <nadav.keren@gmail.com>
2024-11-01 10:40:34 +0200

    Added printing of the message to assert

--- simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/DebugHelpers/Assert.java
+++ simulator/src/main/java/com/github/benmanes/caffeine/cache/simulator/DebugHelpers/Assert.java
@@ -8,6 +8,7 @@ public class Assert {
         if (!cond) {
             Logger logger = System.getLogger("");
             logger.log(Logger.Level.ERROR, msg);
+            System.err.println(msg);
             throw new IllegalStateException(msg);
         }
     }
@@ -17,6 +18,7 @@ public class Assert {
             Logger logger = System.getLogger("");
             String msg = msgSupplier.get();
             logger.log(Logger.Level.ERROR, msg);
+            System.err.println(msgSupplier.get());
             throw new IllegalStateException(msg);
         }
     }
